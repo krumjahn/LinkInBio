facd8f0b47882121b5dae04748c8fd1f34be2d51
krumjahn <rumjahn@gmail.com>
2025-03-01 22:47:25 +0800

    added font awesome

--- src/app/viral-blog-title-generator/page.tsx
+++ src/app/viral-blog-title-generator/page.tsx
@@ -7,6 +7,12 @@ import { GlowEffect } from "../../../components/ui/glow-effect"
 import Link from "next/link"
 import axios from 'axios'
 import { motion } from 'framer-motion'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// Add the icon to the library
+library.add(faWandMagicSparkles)
 
 type TitleFormat = 'whisper' | 'hormozi' | 'specific' | 'tangible';
 
@@ -326,9 +332,7 @@ export default function ViralBlogTitleGenerator() {
                     </div>
                   ) : (
                     <div className="flex items-center justify-center">
-                      <svg className="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 4V20M20 12H4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                      </svg>
+                      <FontAwesomeIcon icon={faWandMagicSparkles} className="mr-2 h-5 w-5" />
                       Generate Viral Titles
                     </div>
                   )}
